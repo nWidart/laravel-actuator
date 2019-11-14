a8901491d3fa444428c1e260c744665c2701bf84
Nicolas Widart <n.widart@gmail.com>
2019-11-14 21:42:09 +0100

    Show status DOWN if application is in maintenance mode

--- src/Health/StatusContributor.php
+++ src/Health/StatusContributor.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace Nwidart\Actuator\Health;
 
+use Illuminate\Contracts\Foundation\Application;
+
 class StatusContributor implements HealthContributor
 {
+    /**
+     * @var Application
+     */
+    private $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
     public function run(): string
     {
-        return 'UP';
+        return $this->app->isDownForMaintenance() ? 'DOWN' : 'UP';
     }
 }
--- tests/Health/StatusContributorTest.php
+++ tests/Health/StatusContributorTest.php
@@ -2,16 +2,38 @@
 
 namespace Nwidart\Actuator\Tests\Health;
 
+use Illuminate\Filesystem\Filesystem;
 use Nwidart\Actuator\Health\StatusContributor;
-use PHPUnit\Framework\TestCase;
+use Orchestra\Testbench\TestCase;
 
 final class StatusContributorTest extends TestCase
 {
+    /**
+     * @var Filesystem
+     */
+    private $finder;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->finder = $this->app['files'];
+    }
+
     /** @test */
     public function it_returns_up(): void
     {
-        $statusContributor = new StatusContributor();
+        $statusContributor = new StatusContributor($this->app);
 
         $this->assertEquals('UP', $statusContributor->run());
+    }
+
+    /** @test */
+    public function it_returns_down_when_app_is_in_maintenance(): void
+    {
+        $this->finder->put($this->app->storagePath() . '/framework/down', 'down');
+        $statusContributor = new StatusContributor($this->app);
+
+        $this->assertEquals('DOWN', $statusContributor->run());
+        $this->finder->delete($this->app->storagePath() . '/framework/down');
     }
 }
